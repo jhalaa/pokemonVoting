f529e87aeed9443294b4a029fa0833281739bff0
Jhalaa <jhalaachinoy@gmail.com>
2019-04-25 03:59:50 -0700

    Adding initial choices

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Choose your favorite pokemon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+            <li>Squirtle</li>
+            <li>Bulbasaur</li>
+            <li>Pikachu</li>
+            <li>Charmander</li>
+        </ul>
       </header>
     </div>
   );
