7b938b41090966ad9ab7c108639a6822536fc731
Jhalaa <jhalaachinoy@gmail.com>
2019-04-26 07:51:48 -0700

    Changing firebase file !

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,18 +1,13 @@
-import app from 'firebase/app';
-
+import * as firebase from 'firebase';
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    databaseURL: "https://voting-app-97ce8.firebaseio.com",
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
-class Firebase {
-    constructor() {
-        app.initializeApp(config);
-    }
-}
-
-export default Firebase;
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+export const pokemon = databaseRef
